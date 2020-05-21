12786306cfc5e6c5dfe49702c40355833f381f66
Your Name <you@example.com>
2020-05-20 19:33:55 -0700

    adding cleaning scripts

---
+++ clean.sh
@@ -0,0 +1,19 @@
+rm CMakeCache.txt
+rm -rf CMakeFiles/
+rm Makefile
+rm build.sh
+rm cmake_install.cmake
+rm cuda_compile_ptx_1_generated_deviceCode.cu.ptx
+rm cuda_compile_ptx_1_generated_deviceCode.cu.ptx_embedded.c
+rm -rf include/
+rm install_manifest.txt
+rm -rf lib/
+rm -rf src/CMakeFiles/
+rm src/Makefile
+rm src/cmake_install.cmake
+rm -rf src/externals/CMakeFiles/
+rm src/externals/Makefile
+rm src/externals/cmake_install.cmake
+rm -rf src/visii/CMakeFiles/
+rm src/visii/Makefile
+rm src/visii/cmake_install.cmake
