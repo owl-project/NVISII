881a9148f017e82c92503cd53ab14a4ae5dcf16e
Your Name <you@example.com>
2020-06-25 11:12:20 -0700

    changes to the scene

--- examples/5.lights.py
+++ examples/5.lights.py
@@ -45,7 +45,7 @@ camera = visii.entity.create(
 camera.get_transform().look_at(
     visii.vec3(0,0,0), # look at (world coordinate)
     visii.vec3(0,0,1), # up vector
-    visii.vec3(-2,-2,1), # camera_origin    
+    visii.vec3(-2,0,1), # camera_origin    
 )
 visii.set_camera_entity(camera)
 
@@ -56,27 +56,28 @@ visii.set_dome_light_intensity(1)
 # lets turn off the ambiant lights 
 visii.set_dome_light_intensity(0)
 
-
 # add a light to an entity
 obj_entity = visii.entity.create(
     name="light_1",
     mesh = visii.mesh.create_sphere('light_1'),
     transform = visii.transform.create("light_1"),
 )
+
 # a light is an entity with a light added to it. 
 obj_entity.set_light(
     visii.light.create('light_1')
 )
-obj_entity.get_light().set_intensity(100000)
+obj_entity.get_light().set_intensity(200000)
 obj_entity.get_light().set_temperature(8000)
 
 #lets set the size and placement of the light
 obj_entity.get_transform().set_scale(
     visii.vec3(0.2)
 )
+
 #light above the scene
 obj_entity.get_transform().set_position(
-    visii.vec3(0,0,3)
+    visii.vec3(0,0,2)
 )
 
 # Second light 
@@ -89,7 +90,7 @@ obj_entity = visii.entity.create(
 obj_entity.set_light(
     visii.light.create('light_2')
 )
-obj_entity.get_light().set_intensity(10000)
+obj_entity.get_light().set_intensity(70000)
 
 # you can also set the light color manually
 obj_entity.get_light().set_color(
@@ -100,9 +101,34 @@ obj_entity.get_transform().set_scale(
     visii.vec3(0.1)
 )
 obj_entity.get_transform().set_position(
-    visii.vec3(0,-0.4,0.3)
+    visii.vec3(0,-0.6,0.4)
+)
+obj_entity.get_transform().set_rotation(
+    visii.angleAxis(90, visii.vec3(0,0,1))
 )
 
+# third light 
+obj_entity = visii.entity.create(
+    name="light_3",
+    mesh = visii.mesh.create_plane('light_3'),
+    transform = visii.transform.create("light_3"),
+)
+obj_entity.set_light(
+    visii.light.create('light_3')
+)
+obj_entity.get_light().set_intensity(10000)
+
+obj_entity.get_light().set_color(
+    visii.vec3(0,1,1)
+)
+obj_entity.get_transform().set_scale(
+    visii.vec3(0.2)
+)
+obj_entity.get_transform().set_position(
+    visii.vec3(0.5,0.5,0.7)
+)
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Lets set some objects in the scene
 entity = visii.entity.create(
@@ -123,9 +149,8 @@ sphere = visii.entity.create(
     transform = visii.transform.create("sphere"),
     material = visii.material.create("sphere")
 )
-# lets set the sphere up
 sphere.get_transform().set_position(
-    visii.vec3(-0.3,0,0.2))
+    visii.vec3(0.4,0,0.2))
 sphere.get_transform().set_scale(
     visii.vec3(0.2))
 sphere.get_material().set_base_color(
@@ -139,13 +164,12 @@ sphere2 = visii.entity.create(
     transform = visii.transform.create("sphere2"),
     material = visii.material.create("sphere2")
 )
-# lets set the sphere2 up
 sphere2.get_transform().set_position(
-    visii.vec3(-0.4,-0.8,0.1))
+    visii.vec3(-0.5,-0.1,0.1))
 sphere2.get_transform().set_scale(
     visii.vec3(0.1))
 sphere2.get_material().set_base_color(
-    visii.vec3(0.1,0.56,0.9))  
+    visii.vec3(0.1,0.56,1))  
 sphere2.get_material().set_roughness(0)   
 sphere2.get_material().set_specular(0)   
 
@@ -155,16 +179,61 @@ sphere3 = visii.entity.create(
     transform = visii.transform.create("sphere3"),
     material = visii.material.create("sphere3")
 )
-# lets set the sphere3 up
 sphere3.get_transform().set_position(
-    visii.vec3(-1,1,0.1))
+    visii.vec3(0.6,-0.5,0.1))
 sphere3.get_transform().set_scale(
     visii.vec3(0.16))
 sphere3.get_material().set_base_color(
     visii.vec3(0.5,0.8,0.5))  
 sphere3.get_material().set_roughness(0)   
-sphere3.get_material().set_specular(0)   
-sphere3.get_material().set_metallic(0)   
+sphere3.get_material().set_specular(1)   
+sphere3.get_material().set_metallic(1)   
+
+cone = visii.entity.create(
+    name="cone",
+    mesh = visii.mesh.create_cone("cone"),
+    transform = visii.transform.create("cone"),
+    material = visii.material.create("cone")
+)
+# lets set the cone up
+cone.get_transform().set_position(
+    visii.vec3(0.08,0.35,0.2))
+cone.get_transform().set_scale(
+    visii.vec3(0.3))
+cone.get_material().set_base_color(
+    visii.vec3(245/255, 230/255, 66/255))  
+cone.get_material().set_roughness(1)   
+cone.get_material().set_specular(0)   
+cone.get_material().set_metallic(0)   
+
+box1 = visii.entity.create(
+    name="box1",
+    mesh = visii.mesh.create_box("box1"),
+    transform = visii.transform.create("box1"),
+    material = visii.material.create("box1")
+)
+# lets set the box1 up
+box1.get_transform().set_position(
+    visii.vec3(-0.5,0.4,0.2))
+box1.get_transform().set_scale(
+    visii.vec3(0.2))
+box1.get_material().set_base_color(
+    visii.vec3(1,1,1))  
+box1.get_material().set_roughness(0)   
+box1.get_material().set_specular(0)   
+box1.get_material().set_metallic(0)   
+box1.get_material().set_transmission(1)   
+
+# lets make the plane light look at the box
+obj_entity = visii.entity.get("light_3")
+obj_entity.get_transform().look_at(
+    box1.get_transform().get_position(),
+    # visii.vec3(-0.5,0.5,0.1),
+    visii.vec3(0,0,1),
+)
+
+# The plane has to be flipped
+obj_entity.get_transform().add_rotation(visii.quat(0,0,1,0))
 
 
 #%%
